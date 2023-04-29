9f20404bdcd0a5dd8e1c17cc8b6ab2e7c8ff029d
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-04-29 23:53:09 +0200

    functionality

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 "use strict";
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
-
+const body = document.querySelector(".body");
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
+});
+
+body.addEventListener("click", function () {
+  menu.classList.remove("is-active");
+  menuLinks.classList.remove("active");
 });
