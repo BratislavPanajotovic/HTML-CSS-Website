3d7be9196864c0e22eadfce40b1e3ae08958419c
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-04-30 00:12:09 +0200

    adding funct

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 "use strict";
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
-const body = document.querySelector(".body");
+const navbar = document.querySelector(".navbar");
+const main = document.querySelector(".main");
+const services = document.querySelector(".services");
+const footer = document.querySelector(".footer__container");
+
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+menuLinks.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
 
-body.addEventListener("click", function () {
-  menu.classList.remove("is-active");
-  menuLinks.classList.remove("active");
+main.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+services.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+footer.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 });
